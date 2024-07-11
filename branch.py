7cfe9a37d01dede1bac394c3374f99ace4672cdc
0111-daisuke <daisuke.mori@fujielab.org>
2024-07-12 08:42:27 +0900

    不要な文の削除

--- branch.py
+++ branch.py
@@ -1,28 +1,31 @@
 from openai import OpenAI
-import os
 
 client = OpenAI()
 
 def branch_api(messages):
     return client.chat.completions.create(
-        model = "gpt-4-1106-preview",
+        model = "gpt-4o",
         messages = messages,
         temperature = 1,
         max_tokens = 5
         )
 
-def main():
+def branch(word):
 
     branch_messages = [
-        {"role": "system", "content": "次の文にことわざが含まれていた場合0を、含まれていない場合1を返してください。"}
+        {"role": "system", "content": f"次の文に{word}が含まれていた場合0を、含まれていない場合1を返してください。"}
         ]
     
     user_input = input("user: ")
     branch_messages.append({"role": "user", "content": user_input})
     branch_response = branch_api(branch_messages)
-    res1 = branch_response.choices[0].message.content
+    res = branch_response.choices[0].message.content
 
-    print(res1)
+    print(res)
+
+def main():
+    word = "ことわざ"
+    branch(word)
 
 # 実行
 if __name__ == "__main__":
